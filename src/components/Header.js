d147ba4686be80ddeda046476e300f67f099e58a
amandhembla@gmail.com <amandhembla@gmail.com>
2018-03-07 11:28:55 +0530

    added in some minor adjustmensts to styles of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {NavLink} from "react-router-dom";
+import {NavLink,Link} from "react-router-dom";
 
 const Header=()=>(
     <div className="header">
-        <h1 className="header__title">Expensify</h1>
-        <div className="header__side">
-            <NavLink  className="header__links" to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-            <NavLink  className="header__links" to="/create" activeClassName="is-active">Add Expense</NavLink>
+        <div className="header__content">
+            <Link to="/"><h1 className="header__title">Expensify</h1></Link>
+            <div className="header__side">
+                <NavLink  className="header__links" to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+                <NavLink  className="header__links" to="/create" activeClassName="is-active">Add Expense</NavLink>
+            </div>
         </div>
     </div>
 );
