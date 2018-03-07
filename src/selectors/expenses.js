58c6b0cf4293020b4bef222ae3aba0cf295a761a
amandhembla@gmail.com <amandhembla@gmail.com>
2018-03-07 12:19:06 +0530

    added in the media queries for th edashboard page

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -3,9 +3,9 @@ import moment from 'moment';
 export default (expenses,{text,sortBy,startDate,endDate})=>{
     return expenses.filter((expense)=>{
         return ( 
-            startDate ? startDate.isSameOrBefore(moment(expense.createdAt),'day') : true &&
-            endDate ? endDate.isSameOrAfter(moment(expense.createdAt),'day') : true &&
-            expense.description.toLowerCase().includes(text.toLowerCase()))
+            (startDate ? startDate.isSameOrBefore(moment(expense.createdAt),'day') : true) &&
+            (endDate ? endDate.isSameOrAfter(moment(expense.createdAt),'day') : true) &&
+            (expense.description.toLowerCase().includes(text.toLowerCase())))
         }).sort((a,b)=>{
             if(sortBy=="date"){
                 return a.createdAt >b.createdAt ? 1: -1;
